9badcd09ecd25531007821ec0a5d20bc712a25b5
Devadoss <prince.devadoss@sap.com>
2018-08-01 19:02:19 +0530

    Fixed Inital Script

--- bin/insertValue.js
+++ bin/insertValue.js
@@ -1,11 +1,12 @@
-let model = require('../server/mongo-db/index');
+let Card = require('../server/cardProcess/api/card/model');
+let mongoose = require('mongoose');
 let Log = require('log');
 let logger = new Log();
 var fs = require('fs');
 
 function initScript() {
     var contentInfo = JSON.parse(fs.readFileSync('bin/consumption/cardDetails.json', 'utf8'));
-    var cardModel = model.getCardModel('card-details');
+    var cardModel = new Card(mongoose).getCardModel();
     logger.info(contentInfo);
     cardModel.insertMany(contentInfo, function(err, data) {
         if(err) {
