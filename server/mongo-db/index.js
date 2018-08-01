3f13a010d586966777ff6244a055a068201d77f8
Devadoss <prince.devadoss@sap.com>
2018-08-01 17:46:17 +0530

    initial Router Request fixed

--- server/mongo-db/index.js
+++ server/mongo-db/index.js
@@ -1,16 +1,8 @@
 var mongoose = require('mongoose');
 var Connection = require('./connect');
-var CardSchema = require('./schema');
-var CardModel = require('./model');
 var mongooseWrapper = {
     init: function(resolve, reject) {
         connection = new Connection(mongoose).init(resolve, reject);
-    },
-    getCardSchema: function() {
-        return new CardSchema(mongoose).getCardSchema();
-    },
-    getCardModel: function(schemaName) {
-        return new CardModel(mongoose).getCardModel(schemaName, this.getCardSchema());
     },
     shutdown: function() {
         mongoose.disconnect();
--- server/mongo-db/model.js
+++
@@ -1,9 +0,0 @@
-function MongoModel(mongoose) {
-    this.mongoose = mongoose;
-}
-
-module.exports = MongoModel;
-
-MongoModel.prototype.getCardModel = function(schemaName, schema) {
-    return this.mongoose.model(schemaName, schema);
-}
